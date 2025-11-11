901511605c9c68bd413b536b85c0bc44e66246c0
Maksym Rybalko <maxymrybalko2005@gmail.com>
2025-11-12 00:31:52 +0200

    add bird sprite and flapping

--- src/index.js
+++ src/index.js
@@ -8,21 +8,52 @@ const config = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 200 }
+      gravity: { y: 400 },
+      debug: true
     }
   },
   scene: {
-    preload: preload,
-    create: create
+    preload,
+    create,
+    update,
   }
 };
+
+const FLAP_VELOCITY = 275;
+const BIRD_INIT_POS = { 
+  x: config.width * 0.1, 
+  y: config.height / 2 
+}
+let bird = null;
 
 new Phaser.Game(config);
 
 function preload () {
-  this.load.image('sky', 'assets/sky.png');
+  this.load.image('sky', 'assets/sky.png')
+  this.load.image('bird', 'assets/bird.png')
 }
 
 function create () {
-  this.add.image(400, 300, 'sky');
+  this.add.image(0, 0, 'sky').setOrigin(0);
+  bird = this.physics.add.sprite(BIRD_INIT_POS.x, BIRD_INIT_POS.y, 'bird').setOrigin(0);
+
+  this.input.on('pointerdown', flap);
+  this.input.keyboard.on('keydown-SPACE', flap);
+}
+
+function update(time, delta) {
+  if (bird.y > config.height - bird.height || bird.y < 0) {
+    restart();
+  }
+}
+
+function restart() {
+  bird.x = BIRD_INIT_POS.x;
+  bird.y = BIRD_INIT_POS.y;
+  
+  bird.body.velocity.y = 0;
+}
+
+function flap() {
+  bird.body.velocity.y = -FLAP_VELOCITY;
 }
